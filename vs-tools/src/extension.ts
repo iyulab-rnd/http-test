6eb57c762043097629a159c07649169b8bf3c8dd
uj <caveman@iyulab.com>
2024-08-05 18:52:20 +0900

    vstool v@1.0.2

--- vs-tools/src/extension.ts
+++ vs-tools/src/extension.ts
@@ -4,45 +4,54 @@ import * as path from 'path';
 
 let outputChannel: vscode.OutputChannel;
 
-export function activate(context: vscode.ExtensionContext) {
-  outputChannel = vscode.window.createOutputChannel('HTTP Test');
-
-  let disposable = vscode.commands.registerCommand('extension.http_test.run', (uri: vscode.Uri) => {
+function runHttpTest(uri: vscode.Uri | undefined, verbose: boolean) {
     const filePath = uri ? uri.fsPath : vscode.window.activeTextEditor?.document.fileName;
     if (!filePath || path.extname(filePath) !== '.http') {
-      vscode.window.showErrorMessage('This command can only be run on .http files.');
-      return;
+        vscode.window.showErrorMessage('This command can only be run on .http files.');
+        return;
     }
 
     const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
     if (!workspaceRoot) {
-      vscode.window.showErrorMessage('No workspace folder is open.');
-      return;
+        vscode.window.showErrorMessage('No workspace folder is open.');
+        return;
     }
 
     outputChannel.clear();
     outputChannel.show(true);
     outputChannel.appendLine('Running HTTP Test...');
 
-    exec(`npx http-test "${filePath}"`, { cwd: workspaceRoot }, (err, stdout, stderr) => {
-      if (stdout) {
-        outputChannel.appendLine(stdout);
-      }
-      if (stderr) {
-        outputChannel.appendLine(`Stderr: ${stderr}`);
-      }
-      if (err) {
-        outputChannel.appendLine(`Error: ${err.message}`);
-      }
-      outputChannel.appendLine('HTTP Test completed.');
+    const verboseFlag = verbose ? '--verbose' : '';
+    exec(`npx http-test "${filePath}" ${verboseFlag}`, { cwd: workspaceRoot }, (err, stdout, stderr) => {
+        if (stdout) {
+            outputChannel.appendLine(stdout);
+        }
+        if (stderr) {
+            outputChannel.appendLine(`Stderr: ${stderr}`);
+        }
+        if (err) {
+            outputChannel.appendLine(`Error: ${err.message}`);
+        }
+        outputChannel.appendLine('HTTP Test completed.');
     });
-  });
+}
 
-  context.subscriptions.push(disposable);
+export function activate(context: vscode.ExtensionContext) {
+    outputChannel = vscode.window.createOutputChannel('HTTP Test');
+
+    let disposableRun = vscode.commands.registerCommand('extension.http_test.run', (uri: vscode.Uri) => {
+        runHttpTest(uri, false);
+    });
+
+    let disposableRunVerbose = vscode.commands.registerCommand('extension.http_test.runVerbose', (uri: vscode.Uri) => {
+        runHttpTest(uri, true);
+    });
+
+    context.subscriptions.push(disposableRun, disposableRunVerbose);
 }
 
 export function deactivate() {
-  if (outputChannel) {
-    outputChannel.dispose();
-  }
+    if (outputChannel) {
+        outputChannel.dispose();
+    }
 }
