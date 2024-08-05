51f064a59dc94c3752bd9ea92491f903b9a7574b
uj <caveman@iyulab.com>
2024-08-05 12:14:45 +0900

    @iyulab/http-test: published to v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,2 @@
-import { run } from './cli';
-
-async function main() {
-  const args = process.argv.slice(2);
-  const filePath = args[0];
-  const options = {
-    verbose: args.includes('--verbose'),
-    var: args.find((arg) => arg.startsWith('--var='))?.split('=')[1]
-  };
-
-  if (!filePath) {
-    console.error('Please provide a file path');
-    process.exit(1);
-  }
-
-  try {
-    await run(filePath, options);
-  } catch (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-}
-
-main();
+export * from './core/HttpFileParser';
+export * from './core/TestManager';
